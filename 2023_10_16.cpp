f5953b3392f96527991a024acd918c602ee7c49c
Myroslav Natalchenko <99477515+MyroslavNatalchenko@users.noreply.github.com>
2023-10-16 16:31:38 +0200

    Update 2023_10_16.cpp

--- 2023_10_16.cpp
+++ 2023_10_16.cpp
@@ -29,32 +29,41 @@ int main() {
     return 0;
 }
 
-
+---------------------------------------------------------------------------------------
+    -----------------------------------------------------------------------------------
+    -----------------------------------------------------------------------------------
 #include <iostream>
+#include <WinDef.h>
 
-bool isEvenOnes(int num) {
-    int count = 0;
-    
-    while (num > 0) {
-        if (num & 1) {
-            count ^= 1;
-        }
-        num >>= 1;
+using namespace std;
+
+bool isEvenOnes(int N)
+{
+    bool cnt=0;
+
+    while (N>0)
+    {
+        if (N&1)
+            cnt^=1;
+        N>>=1;
     }
-    
-    return count == 0;
+
+    if (cnt==0)
+        return 1;
+    else
+        return 0;
 }
 
 int main() {
-    int number;
-    std::cout << "Podaj liczbe: ";
-    std::cin >> number;
-    
-    if (isEvenOnes(number)) {
+    int N;
+    cout << "Podaj liczbe: ";
+    cin >> N;
+
+    if (isEvenOnes(N))
         std::cout << "Liczba jedynek w reprezentacji binarnej jest parzysta." << std::endl;
-    } else {
+    else
         std::cout << "Liczba jedynek w reprezentacji binarnej nie jest parzysta." << std::endl;
-    }
-    
+
     return 0;
 }
+
